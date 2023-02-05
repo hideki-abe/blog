19e3fdfbea6f56fcbe8303405f8b9ea9bbeea368
hidekiabe <hideki-abe@hotmail.com>
2023-02-04 23:10:25 -0300

    Implantação: annotations da entidade Ingredient

--- src/main/java/com/hidekiabe/blog/model/Ingredient.java
+++ src/main/java/com/hidekiabe/blog/model/Ingredient.java
@@ -1,12 +1,26 @@
 package com.hidekiabe.blog.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table( name = "Ingredient")
 public class Ingredient {
 
+    @Id
+    @Column(name = "id")
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "calories")
     private int calories;
 
 
