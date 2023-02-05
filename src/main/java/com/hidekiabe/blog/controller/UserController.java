74778f2f0728d6d876a2617c437a92b67832c0ad
hidekiabe <hideki-abe@hotmail.com>
2023-02-04 23:17:37 -0300

    Implantação: criação do pacote e entidades controller

---
+++ src/main/java/com/hidekiabe/blog/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.hidekiabe.blog.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/blog/users")
+public class UserController {
+}
