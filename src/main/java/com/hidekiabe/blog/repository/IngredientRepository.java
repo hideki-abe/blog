fe05c446455b7bb9f2e3d8ea42acb1d645c2bf6e
hidekiabe <hideki-abe@hotmail.com>
2023-02-04 23:35:32 -0300

    Implantação: criação do pacote e repositórios das entidades

---
+++ src/main/java/com/hidekiabe/blog/repository/IngredientRepository.java
@@ -0,0 +1,9 @@
+package com.hidekiabe.blog.repository;
+
+import com.hidekiabe.blog.model.Ingredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IngredientRepository extends JpaRepository<Ingredient, Integer> {
+}
---
+++ src/main/java/com/hidekiabe/blog/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.hidekiabe.blog.repository;
+
+import com.hidekiabe.blog.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
