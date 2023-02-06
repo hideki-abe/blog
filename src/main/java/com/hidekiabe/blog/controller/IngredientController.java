be0044936a17ee87bc878c31b67550d29dec3cd5
hidekiabe <hideki-abe@hotmail.com>
2023-02-06 02:07:09 -0300

    Atualização: att de endpoints

--- src/main/java/com/hidekiabe/blog/controller/IngredientController.java
+++ src/main/java/com/hidekiabe/blog/controller/IngredientController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/ingredients")
+@RequestMapping("/blog/ingredients")
 public class IngredientController {
 
     private IngredientService service;
--- src/main/java/com/hidekiabe/blog/controller/RecipeController.java
+++ src/main/java/com/hidekiabe/blog/controller/RecipeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/recipes")
+@RequestMapping("/blog/recipes")
 public class RecipeController {
 
     private RecipeService service;
