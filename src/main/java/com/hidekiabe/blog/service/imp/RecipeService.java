a3c1961efbfe5d48e4581b642188e2f7861faef2
hidekiabe <hideki-abe@hotmail.com>
2023-02-06 00:59:22 -0300

    Implantação: correção de RecipeService

--- src/main/java/com/hidekiabe/blog/service/imp/RecipeService.java
+++ src/main/java/com/hidekiabe/blog/service/imp/RecipeService.java
@@ -4,14 +4,17 @@ import com.hidekiabe.blog.model.entity.Ingredient;
 import com.hidekiabe.blog.model.entity.Recipe;
 import com.hidekiabe.blog.repository.RecipeRepository;
 import com.hidekiabe.blog.service.RecipeServiceInterface;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
+@Service
 public class RecipeService implements RecipeServiceInterface {
 
     private RecipeRepository repository;
@@ -22,11 +25,13 @@ public class RecipeService implements RecipeServiceInterface {
     }
 
     @Override
+    @Transactional
     public Recipe save(Recipe recipe) {
         return this.repository.save(recipe);
     }
 
     @Override
+    @Transactional
     public Recipe update(Recipe recipe) {
         Objects.requireNonNull(recipe.getId());
         return repository.save(recipe);
