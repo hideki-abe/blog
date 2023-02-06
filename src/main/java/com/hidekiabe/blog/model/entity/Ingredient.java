ded48cfe0f5d7cc59b5dd4f0b740de6fa7d472eb
hidekiabe <hideki-abe@hotmail.com>
2023-02-06 07:46:49 -0300

    Implantação: validação para entidade Ingredient

--- src/main/java/com/hidekiabe/blog/model/entity/Ingredient.java
+++ src/main/java/com/hidekiabe/blog/model/entity/Ingredient.java
@@ -1,6 +1,8 @@
 package com.hidekiabe.blog.model.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,9 +20,11 @@ public class Ingredient {
     private Integer id;
 
     @Column(name = "name")
+    @NotBlank(message = "Name is mandatory!")
     private String name;
 
     @Column(name = "calories")
+    @NotNull(message = "Calories is mandatory!")
     private Integer calories;
 
 
